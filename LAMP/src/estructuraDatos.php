605befde3259bfa4ee83a83d37263fea8e0ec9a7
a24davidos <a24davidos@iessanclemente.net>
2025-09-26 13:05:05 +0200

    sync casa

---
+++ LAMP/src/estructuraDatos.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+    $datos = [
+        "cocacola" => ["text" => "Coca Cola", "precio" => 2.1],
+        "pepsicola" => ["text"=> "Pepsi Cola", "precio"=>2], 
+        "fantanaranja" => ["text" => "Fanta Naranja", "precio" => 2.5],
+        "trinamanzana" => ["text" => "Trina Manzana", "precio" => 2.3
+    ]
+    ];
+
+?>
+
+</body>
+</html>
