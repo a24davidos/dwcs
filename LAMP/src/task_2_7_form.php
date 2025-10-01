e397088878277a44956c8a527b5ee78923f5f880
DavidOteros <a24davidos@iessanclemente.net>
2025-10-01 20:18:56 +0200

    Corregido problema

--- LAMP/src/task_2_7_form.php
+++ LAMP/src/task_2_7_form.php
@@ -2,91 +2,90 @@
 
 declare(strict_types=1);
 
-$drinks = [
+$datos = [
     "cocacola" => ["text" => "Coca Cola", "precio" => 2.1],
-    "pepsicola" => ["text" => "Pepsi Cola", "precio" => 2.0],
+    "pepsicola" => ["text"=> "Pepsi Cola", "precio"=>2], 
     "fantanaranja" => ["text" => "Fanta Naranja", "precio" => 2.5],
-    "trinamanzana" => ["text" => "Trina Manzana", "precio" => 2.3],
+    "trinamanzana" => ["text" => "Trina Manzana", "precio" => 2.3]
 ];
-?>
 
+function test_input($data){
+    $data = trim($data);
+    $data = stripcslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form</title>
+    <title>Form Sale</title>
     <style>
-        .error {
+        .erro {
             color: #FF0000;
         }
     </style>
 </head>
-
 <body>
+
     <?php
-    function test_input($data)
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
     // define variables and set to empty values
     $number = $drink = "";
     $numberErr = $drinkErr = "";
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        //check required fields
-
-        if (empty($_POST["vDrinks"])) {
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        //check bebidas
+        if(empty($_POST["vDrinks"])){
             $drinkErr = "Drink is required";
         } else {
             $drinkKey = test_input($_POST["vDrinks"]);
         }
 
-        if (empty($_POST["vNumber"])) {
+        //check número de bebidas
+        if (empty($_POST["vNumber"])){
             $numberErr = "Number is required";
-        } else {
-            $number = test_input($_POST["vNumber"]);
-            if ($number <= 0){
-                $numberErr = "A cantidade non é válida!";
-            }
+        } else if ($_POST["vNumber"] <= 0){
+            $numberErr = "Introduce un valor válido";
         }
-
-
-        if ($number && isset($drinks[$drinkKey])) {
-            $drinkText = $drinks[$drinkKey]["text"];
-            $precioUnitario = $drinks[$drinkKey]["precio"];
+        else {
+            $number = test_input($_POST["vNumber"]);
+        }
+        
+        if ($number && isset($datos[$drinkKey])){
+            $drinkText = $datos[$drinkKey]["text"];
+            $precioUnitario = $datos[$drinkKey]["precio"];
             $precioTotal = $number * $precioUnitario;
         } else {
             $precioTotal = 0;
         }
 
+        echo "You have asked for $number bottles of $drinkText. Total price to pay: $precioTotal euros.";
 
-
-        echo "You have asked for $number bottles of $drink. Total price to pay: $precioTotal euros.";
     }
+       
     ?>
-    <h1>FORMS MAIN PAGE</h1>
+
+    <h1>Task 2.7 Form</h1>
+
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <label for="idDrink">Drink: </label>
-        <select name="vDrinks">
-            <?php foreach ($drinks as $key => $drink): ?>
-                <option value="<?= $key ?>">
-                    <?= $drink["text"] ?> (<?= $drink["precio"] ?> €)
-                </option>
-            <?php endforeach; ?>
+        <select name="vDrinks" id="idDrink">
+            <?php
+            foreach ($datos as $key => $value) {
+                echo "<option value='".$key."'>".$value["text"]." (".$value["precio"]."€)</option>";
+            }
+            ?>
         </select>
-        <span class="error"><?php echo $drinkErr; ?></span><br><br>
-
-        <label for="idNumber">Number: </label>
-        <input type="text" id="idNumber" name="vNumber" required><br><br>
-        <span class="error"><?php echo $numberErr; ?></span><br><br>
+        <span class="erro"><?php echo $drinkErr ?></span><br><br><br>
+        
+        <label for="idNumber"> Number: </label>
+        <input type="number" id="idNumber" name="vNumber" required /> <br><br>
+        <span class="erro"><?php echo $numberErr ?></span> <br><br>
         <input type="submit" value="Send">
     </form>
 
 </body>
-
 </html>
