5f710b777f1eae1403ae31efd723945826f2db84
a24davidos <a24davidos@iessanclemente.net>
2025-10-29 10:33:00 +0100

    sync push

--- tema2/task3_3.py
+++ tema2/task3_3.py
@@ -3,5 +3,19 @@
 # It calculates the power using a loop and returns the result.
 # Write a program that invokes the previous function. Use exceptions.
 
-def potencia():
+def potencia(base, exp):
+    if not (type(base) is int or type(exp) is int):
+        raise ValueError("The parameters must be integers")
     
+    producto = 1
+    for numero in range(1,exp +1):
+        producto = producto * base
+    
+    return producto
+
+
+try:
+    print(potencia(3,4))
+    print(potencia(3,4,5))
+except Exception as erro:
+    print(erro)
