500827af8b6d6a5dcfdf62f00dcfcde4950fd6e2
DavidOteros <a24davidos@iessanclemente.net>
2025-11-07 12:34:51 +0100

    Ejercicio formulario

--- form_project/form_project/urls.py
+++ form_project/form_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for password_generator project.
+URL configuration for form_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,11 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path
-from generator import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('passworddd/', views.password, name="password"),
-    path('about/', views.about, name='about'),
+    path('admin/', admin.site.urls),
 ]
--- tema2/password_generator-project/generator/views.py
+++ tema2/password_generator-project/generator/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -6,6 +7,16 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'generator/home.html')
 
+def about(request):
+    return render(request, 'generator/about.html')
+
 
 def password(request):
-    return render(request, 'generator/password.html')
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'))
+    length = int(request.GET.get('length', 8))  # valor por defecto
+    thepassword = '';
+    for x in range (length):
+        thepassword = thepassword + random.choice(characters)
+    return render (request, 'generator/password.html', {'password':thepassword});
