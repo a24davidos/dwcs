fffbe384cf9e96ec136e02a25ae89b9b2fffd1a0
a24davidos <a24davidos@iessanclemente.net>
2025-11-26 09:42:46 +0100

    synt todo

--- Pablo/todoProject/todoProject/settings.py
+++ Pablo/todoProject/todoProject/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'todoProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   # como templates está fuera, static también
+]
 
 LOGIN_URL = '/login'
 
