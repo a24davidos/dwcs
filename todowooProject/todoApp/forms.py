bb010e72b7013dba665439d320486cc43de169dc
a24davidos <a24davidos@iessanclemente.net>
2025-11-28 11:49:23 +0100

    Sync todo

--- todowooProject/todoApp/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Todo
-
-class TodoForm(ModelForm):
-    #Permite crear un formulario a partir 
-    class Meta:
-        model = Todo
-        fields = ['title', 'memo', 'important']
