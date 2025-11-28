4a0beec17b2a7f0006b5d4394ac66627a2bfdb2a
a24davidos <a24davidos@iessanclemente.net>
2025-11-28 12:52:09 +0100

    sync casiña

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+# Register your models here - Así aparece en la página de admin
+admin.site.register(Book)
--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+from django.db.models import Q
+
 
 # Create your views here.
+def home(request):
+    bookTitle = Book.objects.all()[1].title
+    firstBook = get_object_or_404(Book, pk=1)
+    # firstBook = Book.objects.get(id=1) #De esta forma tb se puede hacer pero es mejor con la de arriba
+    # Este es solo válido para uno, mejor usar filtros
+    buscaTitulo = Book.objects.get(title__icontains="atrix")
+    # Queries con multiples resultados
+    bestSellingBooks = Book.objects.filter(is_bestselling=True, rating=5)
+    orExampleBooks = Book.objects.filter(Q(is_bestselling=True) | Q(rating__gte=3))
+
+    # Ejercicio con highestRating
+    bestRating = Book.objects.filter(rating=5)
+    # Books have pattern Potter y bestselling o tiene un rate de 3
+    best3 = Book.objects.filter(
+        Q(title__icontains="Potter", is_bestselling=True) | Q(rating=3)
+    )
+    # Mas vendidos y autor "J.K. Transfoba"
+    bestSellingTransfoba = Book.objects.filter(
+        is_bestselling=True, title__icontains="J.K. Transfoba"
+    )
+
+    return render(
+        request,
+        "book_outlet/home.html",
+        {
+            "title": bookTitle,
+            "firstBook": firstBook,
+            "buscaTitulo": buscaTitulo,
+            "bestSellingBooks": bestSellingBooks,
+            "orExampleBooks": orExampleBooks,
+        },
+    )
