58227be16b6e57fc19c0b2c82723488cb63ac8e4
a24davidos <a24davidos@iessanclemente.net>
2025-12-03 09:30:21 +0100

    subir todo

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Address, Country
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_filter = ("author", "rating")
+
 
 # Register your models here - Así aparece en la página de admin
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,13 +1,56 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+
 # Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=50)
-    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
-    is_bestselling = models.BooleanField(default=False) #We are setting a default value
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
 
     def __str__(self):
-        return f'{self.title} ({self.rating})'
-    
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.street}, {self.postal_code},{self.city}"
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.SET_NULL, null=True)
+
+    # Definir to String
+    def __str__(self):
+        return f"{self.first_name}({self.last_name})"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="fkbooks"
+    )
+
+    is_bestselling = models.BooleanField(
+        default=False
+    )  # We are setting a default value
+    published_countries = models.ManyToManyField(Country)
+
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
