edbd25adff7d74778179a2197eb4dd005f995d85
DavidOteros <a24davidos@iessanclemente.net>
2025-12-04 19:44:44 +0100

    sync cole

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -116,7 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+
+STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'static'   # comentar mientras estés en desarrollo
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
